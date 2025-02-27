0909f190a6056301ff34b29a08efa483464a09b5
YPZhou <yunpeng_zhou@163.com>
2025-02-27 20:12:16 +0800

    Test draw game board in CLI

---
+++ Harmonies_CLI/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Harmonies_CLI
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Yellow board
+			//Console.WriteLine("[------]        [------]        [------]");
+			//Console.WriteLine("[      ]        [      ]        [      ]");
+			//Console.WriteLine("[      ][------][      ][------][      ]");
+			//Console.WriteLine("[======][      ][======][      ][======]");
+			//Console.WriteLine("[------][      ][------][      ][------]");
+			//Console.WriteLine("[      ][======][      ][======][      ]");
+			//Console.WriteLine("[      ][------][      ][------][      ]");
+			//Console.WriteLine("[======][      ][======][      ][======]");
+			//Console.WriteLine("[------][      ][------][      ][------]");
+			//Console.WriteLine("[      ][======][      ][======][      ]");
+			//Console.WriteLine("[      ][------][      ][------][      ]");
+			//Console.WriteLine("[======][      ][======][      ][======]");
+			//Console.WriteLine("[------][      ][------][      ][------]");
+			//Console.WriteLine("[      ][======][      ][======][      ]");
+			//Console.WriteLine("[      ][------][      ][------][      ]");
+			//Console.WriteLine("[======][      ][======][      ][======]");
+			//Console.WriteLine("[------][      ][------][      ][------]");
+			//Console.WriteLine("[      ][======][      ][======][      ]");
+			//Console.WriteLine("[      ]        [      ]        [      ]");
+			//Console.WriteLine("[======]        [======]        [======]");
+
+			// Blue board
+			Console.WriteLine("[------]        [------]        [------]        [------]");
+			Console.WriteLine("[      ]        [      ]        [      ]        [      ]");
+			Console.WriteLine("[      ][------][      ][------][      ][------][      ]");
+			Console.WriteLine("[======][      ][======][      ][======][      ][======]");
+			Console.WriteLine("[------][      ][------][      ][------][      ][------]");
+			Console.WriteLine("[      ][======][      ][======][      ][======][      ]");
+			Console.WriteLine("[      ][------][      ][------][      ][------][      ]");
+			Console.WriteLine("[======][      ][======][      ][======][      ][======]");
+			Console.WriteLine("[------][      ][------][      ][------][      ][------]");
+			Console.WriteLine("[      ][======][      ][======][      ][======][      ]");
+			Console.WriteLine("[      ][------][      ][------][      ][------][      ]");
+			Console.WriteLine("[======][      ][======][      ][======][      ][======]");
+			Console.WriteLine("[------][      ][------][      ][------][      ][------]");
+			Console.WriteLine("[      ][======][      ][======][      ][======][      ]");
+			Console.WriteLine("[      ]        [      ]        [      ]        [      ]");
+			Console.WriteLine("[======]        [======]        [======]        [======]");
+		}
+	}
+}
