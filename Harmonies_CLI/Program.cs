bea007416feb1bd2de46d6885ec5591eb3cbd9e0
YPZhou <yunpeng_zhou@163.com>
2025-02-27 21:09:59 +0800

    Setup hexagon coordinates

--- Harmonies_CLI/Program.cs
+++ Harmonies_CLI/Program.cs
@@ -6,41 +6,41 @@
 		{
 			// Yellow board
 			Console.WriteLine(" [----]        [----]        [----]");
-			Console.WriteLine("[      ]      [      ]      [      ]");
+			Console.WriteLine("[ 0  0 ]      [ 2  1 ]      [ 4  2 ]");
 			Console.WriteLine("[      ][----][      ][----][      ]");
-			Console.WriteLine(" [====][      ][====][      ][====]");
+			Console.WriteLine(" [====][ 1  1 ][====][ 3  2 ][====]");
 			Console.WriteLine(" [----][      ][----][      ][----]");
-			Console.WriteLine("[      ][====][      ][====][      ]");
+			Console.WriteLine("[ 0  1 ][====][ 2  2 ][====][ 4  3 ]");
 			Console.WriteLine("[      ][----][      ][----][      ]");
-			Console.WriteLine(" [====][      ][====][      ][====]");
+			Console.WriteLine(" [====][ 1  2 ][====][ 3  3 ][====]");
 			Console.WriteLine(" [----][      ][----][      ][----]");
-			Console.WriteLine("[      ][====][      ][====][      ]");
+			Console.WriteLine("[ 0  2 ][====][ 2  3 ][====][ 4  4 ]");
 			Console.WriteLine("[      ][----][      ][----][      ]");
-			Console.WriteLine(" [====][      ][====][      ][====]");
+			Console.WriteLine(" [====][ 1  3 ][====][ 3  4 ][====]");
 			Console.WriteLine(" [----][      ][----][      ][----]");
-			Console.WriteLine("[      ][====][      ][====][      ]");
+			Console.WriteLine("[ 0  3 ][====][ 2  4 ][====][ 4  5 ]");
 			Console.WriteLine("[      ][----][      ][----][      ]");
-			Console.WriteLine(" [====][      ][====][      ][====]");
+			Console.WriteLine(" [====][ 1  4 ][====][ 3  5 ][====]");
 			Console.WriteLine(" [----][      ][----][      ][----]");
-			Console.WriteLine("[      ][====][      ][====][      ]");
+			Console.WriteLine("[ 0  4 ][====][ 2  5 ][====][ 4  6 ]");
 			Console.WriteLine("[      ]      [      ]      [      ]");
 			Console.WriteLine(" [====]        [====]        [====]");
 
 			// Blue board
 			//Console.WriteLine(" [----]        [----]        [----]        [----]");
-			//Console.WriteLine("[      ]      [      ]      [      ]      [      ]");
+			//Console.WriteLine("[ 0  0 ]      [ 2  1 ]      [ 4  2 ]      [ 6  3 ]");
 			//Console.WriteLine("[      ][----][      ][----][      ][----][      ]");
-			//Console.WriteLine(" [====][      ][====][      ][====][      ][====]");
+			//Console.WriteLine(" [====][ 1  1 ][====][ 3  2 ][====][ 5  3 ][====]");
 			//Console.WriteLine(" [----][      ][----][      ][----][      ][----]");
-			//Console.WriteLine("[      ][====][      ][====][      ][====][      ]");
+			//Console.WriteLine("[ 0  1 ][====][ 2  2 ][====][ 4  3 ][====][ 6  4 ]");
 			//Console.WriteLine("[      ][----][      ][----][      ][----][      ]");
-			//Console.WriteLine(" [====][      ][====][      ][====][      ][====]");
+			//Console.WriteLine(" [====][ 1  2 ][====][ 3  3 ][====][ 5  4 ][====]");
 			//Console.WriteLine(" [----][      ][----][      ][----][      ][----]");
-			//Console.WriteLine("[      ][====][      ][====][      ][====][      ]");
+			//Console.WriteLine("[ 0  2 ][====][ 2  3 ][====][ 4  4 ][====][ 6  5 ]");
 			//Console.WriteLine("[      ][----][      ][----][      ][----][      ]");
-			//Console.WriteLine(" [====][      ][====][      ][====][      ][====]");
+			//Console.WriteLine(" [====][ 1  3 ][====][ 3  4 ][====][ 5  5 ][====]");
 			//Console.WriteLine(" [----][      ][----][      ][----][      ][----]");
-			//Console.WriteLine("[      ][====][      ][====][      ][====][      ]");
+			//Console.WriteLine("[ 0  3 ][====][ 2  4 ][====][ 4  5 ][====][ 6  6 ]");
 			//Console.WriteLine("[      ]      [      ]      [      ]      [      ]");
 			//Console.WriteLine(" [====]        [====]        [====]        [====]");
 		}
